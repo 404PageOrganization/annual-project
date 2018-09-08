03cc8fca54f36b9a9ec97b3989c5d12590ebfe42
cometeme <cometemeJackson@outlook.com>
2018-09-08 18:44:44 +0800

    update L to LA

--- font_to_png.py
+++ font_to_png.py
@@ -3,9 +3,9 @@
 from PIL import Image, ImageDraw, ImageFont
 import os
 
-base_dir = '/Users/adelard/Documents/annual_project/'
-fonts_dir = '/Users/adelard/Documents/annual_project/fonts/'
-output_dir = '/Users/adelard/Documents/annual_project/output/'
+base_dir = ''
+fonts_dir = ''
+output_dir = ''
 
 characters = open(base_dir + 'test.txt', 'r').read().replace('\n', '')
 
@@ -14,7 +14,7 @@ for font_name in list(os.listdir(fonts_dir))[1:]:
     font = ImageFont.truetype(fonts_dir + font_name, 96)
 
     for output_text in characters:
-        img = Image.new(mode='L', size=(128, 128), color=255)
+        img = Image.new(mode='LA', size=(128, 128), color=255)
         draw = ImageDraw.Draw(img)
         draw.text((0, 0), output_text, font=font)
 
