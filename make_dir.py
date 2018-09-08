dcf7ea2abd4ef98c34dff5cb68589c2fb4ae7f90
cometeme <cometemeJackson@outlook.com>
2018-09-08 20:55:45 +0800

    change to alpha img

--- make_dir.py
+++ make_dir.py
@@ -1,9 +1,10 @@
+# coding utf-8
+
 import os
 
-base_dir = '/Users/adelard/Documents/annual_project/'
-output_dir = '/Users/adelard/Documents/annual_project/output/'
+output_dir = 'output/'
 
-characters = open(base_dir + 'characters.txt', 'r').read().replace('\n', '')
+characters = open('test.txt', 'r').read().replace('\n', '')
 
 for output_text in characters:
     os.system('mkdir ' + output_dir + output_text)
