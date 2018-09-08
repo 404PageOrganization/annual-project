d4fa3dbc1c742d27143cc289eb336d0b0015376e
cometeme <cometemeJackson@outlook.com>
2018-09-08 21:10:57 +0800

    add .gitignore

--- font_to_png.py
+++ font_to_png.py
@@ -3,18 +3,30 @@
 from PIL import Image, ImageDraw, ImageFont
 import os
 
+# Define the abspath
 fonts_dir = 'fonts/'
 output_dir = 'output/'
 
+# Read 3500 most used chinese characters
 characters = open('test.txt', 'r').read().replace('\n', '')
 
-
+# Use all fonts in fonts' directory
+# The first item in list is a file named ".DS_Store", not a text file
 for font_name in list(os.listdir(fonts_dir))[1:]:
+
+    # Read font by using truetype
     font = ImageFont.truetype(fonts_dir + font_name, 96)
 
+    # Traverse all characters
     for output_text in characters:
+
+        # Create a 128x128 img
         img = Image.new(mode='LA', size=(128, 128))
+
         draw = ImageDraw.Draw(img)
+
+        # todo: Make the font draw on center
+
         draw.text((0, 0), output_text, font=font, fill=(0, 255))
 
         img.save(output_dir + output_text + '/' +
