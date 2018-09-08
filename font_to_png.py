3bf797ca86058151b7b05dc5895503184db0ad92
cometeme <cometemeJackson@outlook.com>
2018-09-08 20:55:45 +0800

    change to alpha img
    Former-commit-id: dcf7ea2abd4ef98c34dff5cb68589c2fb4ae7f90

--- font_to_png.py
+++ font_to_png.py
@@ -3,20 +3,19 @@
 from PIL import Image, ImageDraw, ImageFont
 import os
 
-base_dir = ''
-fonts_dir = ''
-output_dir = ''
+fonts_dir = 'fonts/'
+output_dir = 'output/'
 
-characters = open(base_dir + 'test.txt', 'r').read().replace('\n', '')
+characters = open('test.txt', 'r').read().replace('\n', '')
 
 
 for font_name in list(os.listdir(fonts_dir))[1:]:
     font = ImageFont.truetype(fonts_dir + font_name, 96)
 
     for output_text in characters:
-        img = Image.new(mode='LA', size=(128, 128), color=255)
+        img = Image.new(mode='LA', size=(128, 128))
         draw = ImageDraw.Draw(img)
-        draw.text((0, 0), output_text, font=font)
+        draw.text((0, 0), output_text, font=font, fill=(0, 255))
 
         img.save(output_dir + output_text + '/' +
-                 font_name.replace('.ttf', '') + '.png', "PNG")
+                 font_name.replace('.ttf', '.png').replace('.ttc', '.png'), "PNG")
