6c86858086d77568c9100f67bab6dab69a27cee4
MingZZX <41257880+ming-zzx@users.noreply.github.com>
2018-09-10 19:57:39 +0800

    improve compatibility
    Former-commit-id: 7e11230fa5992b463acc1405c040fcf24808bb71

--- make_dir.py
+++ make_dir.py
@@ -9,6 +9,8 @@ output_dir = 'output'
 # Read 3500 most used chinese characters
 characters = open('test.txt', 'r', encoding='utf-8').read().replace('\n', '')
 
-# Make all directorys
+# Make all directories
 for output_text in characters:
-    os.mkdir(output_dir + os.sep + output_text)
+    dir =  output_dir + os.sep + output_text
+    if not os.path.exists(dir):
+        os.mkdir(dir)
