765866882675bc933e71bf08a8dadad73f87bc06
cometeme <cometemeJackson@outlook.com>
2018-09-14 20:20:46 +0800

    update .DS_Store settings
    Former-commit-id: e222f03a9ed92f739b035cad463bfac5f2d4878d

--- font_to_png.py
+++ font_to_png.py
@@ -11,8 +11,8 @@ output_dir = 'output'
 characters = open('test.txt', 'r', encoding='utf-8').read().replace('\n', '')
 
 # Use all fonts in fonts' directory
-# The first item in list is a file named ".DS_Store", not a font file
-for font_name in list(os.listdir(fonts_dir))[1:]:
+# One item in list is a file named ".DS_Store", not a font file, so ignore it
+for font_name in [name for name in os.listdir(fonts_dir) if name != '.DS_Store']:
 
     # Read font by using truetype
     font = ImageFont.truetype(fonts_dir + os.sep + font_name, 96)
@@ -26,10 +26,11 @@ for font_name in list(os.listdir(fonts_dir))[1:]:
         draw = ImageDraw.Draw(img)
 
         # Make the font drawn on center
-        text_size=draw.textsize(output_text, font)
-        text_w=text_size[0]
-        text_h=text_size[1]
-        draw.text((64-text_w/2, 64-text_h/2), output_text, font=font, fill=(0, 255))
+        text_size = draw.textsize(output_text, font)
+        text_w = text_size[0]
+        text_h = text_size[1]
+        draw.text((64 - text_w / 2, 64 - text_h / 2),
+                  output_text, font=font, fill=(0, 255))
 
         img.save(output_dir + os.sep + output_text + os.sep +
                  font_name.replace('.ttf', '.png').replace('.ttc', '.png'), "PNG")
