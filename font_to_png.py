80f2f6ed866656014c837090fbc8e01d461aef85
cometeme <cometemeJackson@outlook.com>
2018-09-19 22:45:58 +0800

    update translate.py

--- font_to_png.py
+++ font_to_png.py
@@ -21,7 +21,7 @@ for font_name in [name for name in os.listdir(fonts_dir) if name != '.DS_Store']
     for output_text in characters:
 
         # Create a L with alpha img
-        img = Image.new(mode='LA', size=(128, 128))
+        img = Image.new(mode='L', size=(128, 128), color=255)
 
         draw = ImageDraw.Draw(img)
 
@@ -30,7 +30,7 @@ for font_name in [name for name in os.listdir(fonts_dir) if name != '.DS_Store']
         text_w = text_size[0]
         text_h = text_size[1]
         draw.text((64 - text_w / 2, 64 - text_h / 2),
-                  output_text, font=font, fill=(0, 255))
+                  output_text, font=font, fill=0)
 
         # Save the image
         img.save(raw_img_dir + os.sep + output_text + os.sep +
