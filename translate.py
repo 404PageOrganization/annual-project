18ee8acb83943c790d5fcaf1459a9fd11417b6cc
cometeme <cometemeJackson@outlook.com>
2018-09-19 13:07:57 +0800

    update translate

--- translate.py
+++ translate.py
@@ -7,9 +7,6 @@ import numpy
 
 output_dir = 'output'
 
-font_len = 13
-characters_count = 40
-
 images = []
 label = []
 
@@ -26,7 +23,54 @@ x = x.reshape(x.shape[0], 128, 128, 2).astype('float32') / 255
 y = np_utils.to_categorical(y)
 
 Generative = Sequential([
-
+    Conv2D(input_shape=(128, 128, 2),
+           filters=12,
+           kernel_size=3,
+           padding='same',
+           activation='relu'),
+    MaxPooling2D(pool_size=2),
+    Conv2D(filters=24,
+           kernel_size=3,
+           padding='same',
+           activation='relu'),
+    MaxPooling2D(pool_size=2),
+    Conv2D(filters=48,
+           kernel_size=3,
+           padding='same',
+           activation='relu'),
+    MaxPooling2D(pool_size=2),
+    Flatten(),
+    Dense(input_shape=(49152,),
+          units=2048,
+          kernel_initializer='normal',
+          activation='relu'),
+    Dense(units=128,
+          kernel_initializer='normal',
+          activation='relu'),
+    Dense(units=128,
+          kernel_initializer='normal',
+          activation='relu'),
+    Dense(units=2048,
+          kernel_initializer='normal',
+          activation='relu'),
+    Dense(units=49152,
+          kernel_initializer='normal',
+          activation='relu'),
+    Reshape((32, 32, 48), input_shape=(49152,)),
+    UpSampling(2),
+    Conv2D(input_shape=(32, 32, 48),
+           filters=24,
+           kernel_size=3,
+           padding='same',
+           activation='relu'),
+    Conv2D(filters=12,
+           kernel_size=3,
+           padding='same',
+           activation='relu'),
+    Conv2D(filters=2,
+           kernel_size=3,
+           padding='same',
+           activation='relu'),
 ])
 
 Discriminative = Sequential([
@@ -50,14 +94,14 @@ Discriminative = Sequential([
     Dropout(0.25),
     MaxPooling2D(pool_size=2),
     Flatten(),
-    Dense(units=1600,
+    Dense(units=2048,
           kernel_initializer='normal',
           activation='relu'),
     Dropout(0.5),
-    Dense(units=400,
+    Dense(units=16,
           kernel_initializer='normal',
           activation='relu'),
-    Dense(units=40,
+    Dense(units=2,
           kernel_initializer='normal',
           activation='softmax')
 ])
