b965fc2394566f63aac410075463d59975ee1087
MingZZX <41257880+ming-zzx@users.noreply.github.com>
2018-09-19 13:17:34 +0800

    use glob to list fonts
    Former-commit-id: 2c8788391ed349fc0fae260d7de1ca006a7a334a

--- font_list.py
+++ font_list.py
@@ -1,25 +1,27 @@
 # coding utf-8
 
 import os
+import glob
 
 # Define directories here
 fonts_dir = 'fonts'
 fonts_reserve_dir = 'fonts_reserve'
 
 # Open the markdown file
-f = open('font_reference.md', 'w')
+f = open('font_reference.md', 'w', encoding='utf-8')
 
 f.write('### 字体引用目录\n\n')
 
 # Write font reference
-for font_name in [name for name in os.listdir(fonts_dir) if name != '.DS_Store']:
-    font_name = font_name.replace('.ttf', '').replace('.ttc', '')
+for font_name in glob.glob(fonts_dir + '/*.ttf'):
+    font_name = font_name.replace(
+        fonts_dir + os.sep, '').replace('.ttf', '')
     f.write('-   ' + font_name + '\n')
 
 f.write('\n### 备用字体\n\n')
 
 # Write font_reserve reference
-for font_reserve_name in [name for name in os.listdir(fonts_reserve_dir) if name != '.DS_Store']:
+for font_reserve_name in glob.glob(fonts_reserve_dir + '/*.ttf'):
     font_reserve_name = font_reserve_name.replace(
-        '.ttf', '').replace('.ttc', '')
+        fonts_reserve_dir + os.sep, '').replace('.ttf', '')
     f.write('-   ' + font_reserve_name + '\n')
