5517ce73315d330a8c040f7ea522ecebed0e3709
cometeme <cometemeJackson@outlook.com>
2018-09-20 13:12:59 +0800

    rename files

--- real_font_to_png.py
+++ real_font_to_png.py
@@ -38,28 +38,3 @@ for output_text in characters:
     # Save the image
     img.save(real_img_dir + os.sep + output_text + os.sep +
              font_name.replace('.ttf', '.png').replace('.ttc', '.png'), "PNG")
-
-# Use other fonts in fonts' directory
-for font_name in font_list[1:]:
-
-    # Read font by using truetype
-    font = ImageFont.truetype(fonts_dir + os.sep + font_name, 96)
-
-    # Traverse all characters
-    for output_text in characters:
-
-        # Create a L with alpha img
-        img = Image.new(mode='LA', size=(128, 128), color=(255, 0))
-
-        draw = ImageDraw.Draw(img)
-
-        # Make the font drawn on center
-        text_size = draw.textsize(output_text, font)
-        text_w = text_size[0]
-        text_h = text_size[1]
-        draw.text((64 - text_w / 2, 64 - text_h / 2),
-                  output_text, font=font, fill=(0, 255))
-
-        # Save the image
-        img.save(raw_img_dir + os.sep + output_text + os.sep +
-                 font_name.replace('.ttf', '.png').replace('.ttc', '.png'), "PNG")
