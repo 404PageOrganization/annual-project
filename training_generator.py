9d7127f783d8551cf5bc6d1ad5bf41c83e09c30c
cometeme <cometemeJackson@outlook.com>
2018-12-11 20:48:18 +0800

    update model 1. Only save small batch of imgs 2. Change learning rate 3. Use 'Save best' checkpoint 4. Use callbacks instead of for loops
    Former-commit-id: 1a5b33c55c6085286098b683eb6238a5e3ff6e05

--- training_generator.py
+++ training_generator.py
@@ -1,10 +1,10 @@
 from keras.utils import np_utils
 from keras.regularizers import l2
-from keras.models import Sequential, load_model
+from keras.models import Sequential
 from keras.layers import BatchNormalization, Conv2D, UpSampling2D, PReLU
+from keras.callbacks import ModelCheckpoint, Callback
+from keras.optimizers import Adam
 from PIL import Image, ImageDraw, ImageFont
-from colorama import init, Fore, Style
-import pickle
 import os
 import numpy
 
@@ -13,40 +13,19 @@ import numpy
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
-# Init colorama
-init(autoreset=True)
-
-
 # Define abspaths
 fonts_dir = 'fonts'
 raw_img_dir = 'raw_img'
 real_img_dir = 'real_img'
 fake_img_dir = 'fake_img'
-model_data_dir = 'model_data'
-data_file_name = 'model.pickle'
+model_data_dir = 'model_data/model.h5'
 
 
-# Define running args
-run_epochs = 10
+# Define hyperparameters
 epochs_for_generator = 200
-save_model_rate = 5
-
-# Define model args
+save_image_rate = 1
+learning_rate = 0.05
 l2_rate = 0.01
-
-
-# Load model datas
-trained = os.path.exists(model_data_dir + '/' + data_file_name)
-
-if trained:
-    model_data = open(model_data_dir + '/' + data_file_name, 'rb')
-    start_epoch = pickle.load(model_data)
-    generatorr_initial_epoch = pickle.load(model_data)
-    model_data.close()
-else:
-    start_epoch = 0
-    generatorr_initial_epoch = 0
-    discriminator_initial_epoch = 0
 
 
 # Read real images & characters
@@ -99,8 +78,13 @@ real_images = real_images.reshape(
     real_images.shape[0], 128, 128, 2).astype('float32') / 127.5 - 1
 
 
+# Use only 20 of characters to save
+batch_raw_images = raw_images[:20]
+batch_real_images = real_images[:20]
+batch_characters = characters[:20]
+
 # Save sample images
-for character, real_image, raw_image in zip(characters, real_images, raw_images):
+for character, real_image, raw_image in zip(batch_characters, batch_real_images, batch_raw_images):
     real_sample = ((real_image + 1) * 127.5).astype('uint8')
     raw_sample = ((raw_image + 1) * 127.5).astype('uint8')
     Image.fromarray(real_sample, mode='LA').save(
@@ -108,13 +92,13 @@ for character, real_image, raw_image in zip(characters, real_images, raw_images)
     Image.fromarray(raw_sample, mode='LA').save(
         fake_img_dir + '/' + character + 'raw_img.png')
 
+
 # Define the models
 generator = Sequential([
-    Conv2D(input_shape=(128, 128, 1),
+    Conv2D(input_shape=(128, 128, 2),
            filters=8,
            kernel_size=3,
            strides=2,
-           # kernel_initializer='uniform',
            kernel_regularizer=l2(l2_rate),
            padding='same'),
     PReLU(),
@@ -122,7 +106,6 @@ generator = Sequential([
     Conv2D(filters=16,
            kernel_size=3,
            strides=2,
-           # kernel_initializer='uniform',
            kernel_regularizer=l2(l2_rate),
            padding='same'),
     PReLU(),
@@ -130,14 +113,12 @@ generator = Sequential([
     Conv2D(filters=32,
            kernel_size=5,
            strides=2,
-           # kernel_initializer='uniform',
            kernel_regularizer=l2(l2_rate),
            padding='same'),
     PReLU(),
     BatchNormalization(),
     Conv2D(filters=32,
            kernel_size=5,
-           # kernel_initializer='uniform',
            kernel_regularizer=l2(l2_rate),
            padding='same'),
     PReLU(),
@@ -145,7 +126,6 @@ generator = Sequential([
     BatchNormalization(),
     Conv2D(filters=16,
            kernel_size=3,
-           # kernel_initializer='uniform',
            kernel_regularizer=l2(l2_rate),
            padding='same'),
     PReLU(),
@@ -153,15 +133,13 @@ generator = Sequential([
     BatchNormalization(),
     Conv2D(filters=8,
            kernel_size=3,
-           # kernel_initializer='uniform',
            kernel_regularizer=l2(l2_rate),
            padding='same'),
     PReLU(),
     UpSampling2D(size=2),
     BatchNormalization(),
-    Conv2D(filters=1,
+    Conv2D(filters=2,
            kernel_size=3,
-           # kernel_initializer='uniform',
            kernel_regularizer=l2(l2_rate),
            padding='same',
            activation='tanh'),
@@ -171,51 +149,35 @@ generator = Sequential([
 # Print model struct
 print(generator.summary())
 
-# Load trained models
-if trained:
-    generator = load_model(model_data_dir + '/generator.h5')
 
 # Compile models
 generator.compile(loss='logcosh',
-                  optimizer='RMSProp',
+                  optimizer=Adam(lr=learning_rate),
                   metrics=['acc'])
 
-# Train models
-for epoch in range(start_epoch + 1, start_epoch + run_epochs + 1):
-    print(Fore.BLUE + Style.BRIGHT + 'Epoch:{}'.format(epoch))
 
-    length = len(characters)
+# Set callbacks
+checkpoint = ModelCheckpoint(model_data_dir, save_best_only=True)
 
-    # Training generator
 
-    generator.fit(x=raw_images,
-                  y=real_images,
-                  initial_epoch=generatorr_initial_epoch,
-                  epochs=generatorr_initial_epoch + epochs_for_generator,
-                  verbose=2)
+class save_fake_img(Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if(epoch % save_image_rate == 0):
+            print('Saving fake images.')
+            fake_images = generator.predict(x=batch_raw_images, verbose=1)
+            for character, fake_image in zip(batch_characters, fake_images):
+                save_image = ((fake_image + 1) * 127.5).astype('uint8')
+                Image.fromarray(save_image, mode='LA').save(
+                    fake_img_dir + '/' + character + str(epoch) + '.png')
 
-    generatorr_initial_epoch += epochs_for_generator
 
-    # Save fake images
-    print(Fore.BLUE + Style.BRIGHT + 'Generating fake images.')
-    fake_images = generator.predict(x=raw_images, verbose=1)
+save_img = save_fake_img()
 
-    for character, fake_image in zip(characters, fake_images):
-        save_image = ((fake_image + 1) * 127.5).astype('uint8')
-        Image.fromarray(save_image, mode='LA').save(
-            fake_img_dir + '/' + character + str(generatorr_initial_epoch) + '.png')
 
-    print(Fore.GREEN + Style.BRIGHT + 'Image saved.')
-
-    # Save models
-    if(epoch % save_model_rate == 0):
-        # Write now epoch
-        model_data = open(model_data_dir + '/' + data_file_name, 'wb')
-        pickle.dump(epoch, model_data)
-        pickle.dump(generatorr_initial_epoch, model_data)
-        model_data.close()
-
-        # Save model
-        generator.save(model_data_dir + '/generator.h5')
-
-        print(Fore.GREEN + Style.BRIGHT + 'Models saved.')
+# Training generator
+generator.fit(x=raw_images,
+              y=real_images,
+              epochs=epochs_for_generator,
+              verbose=2,
+              callbacks=[checkpoint, save_img],
+              validation_split=0.2)
