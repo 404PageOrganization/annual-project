9d43d174496092077d4d038848de254b190b522c
mzzx <41257880+mzzx@users.noreply.github.com>
2019-03-03 09:59:36 +0800

    Update style_prediction.py
    Former-commit-id: 6f6cc2bdab723e234f8ac7a40f3f75a52eb7ae13

--- style_prediction.py
+++ style_prediction.py
@@ -14,14 +14,18 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 real_img_dir = 'real_img/'
 real_imgs = []
 
+
+# Load real images
 for real_img_file in [name for name in os.listdir(real_img_dir) if name[0] != '.']:
-    img = Image.open(real_img_dir + real_img_file).convert('L')
+    img = Image.open(real_img_dir + real_img_file)
     real_imgs.append(list(img.getdata()))
 
 real_imgs = numpy.array(real_imgs)
 real_imgs = real_imgs.reshape(
     real_imgs.shape[0], 128, 128, 1).astype('float32') / 255
 
+
+# Load model and predict the style
 model = load_model('model_data/style_discriminator.h5',
                    {'GlobalStandardPooling2D': GlobalStandardPooling2D})
 
