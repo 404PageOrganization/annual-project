5fc3701513936b70de7692946beb271e998f46ac
mzzx <41257880+mzzx@users.noreply.github.com>
2019-03-03 09:35:53 +0800

    Update image_mosaicking.py
    Former-commit-id: 525191d9260042f7a74aa5228520ca32370aff29

--- image_mosaicking.py
+++ image_mosaicking.py
@@ -3,23 +3,26 @@
 import os
 from PIL import Image, ImageDraw, ImageFont
 
+
 in_dir = 'fake_img'
 out_dir = 'fake_img_mosaicking'
-fake_img_name = []
+
+
+fake_img_num = 0
 chars = []
-
-img_num = 1
-i = 0
-rate = 100
-
-for fake_img_file in [name for name in os.listdir(in_dir) if name[0] != '.']:
-    fake_img_name.append(fake_img_file)
+for fake_img_file in [name for name in os.listdir(in_dir) if (name[0] != '.' and name[0] != 'T')]:
+    fake_img_num += 1
     name = fake_img_file[0]
     if name not in chars:
         chars.append(name)
 
-row_num = len(fake_img_name) // len(chars)
+
+row_num = fake_img_num // len(chars)
 col_num = 5
+img_num = 1
+i = 0
+rate = 10
+
 
 img_new = Image.new(mode='L', size=(
     row_num * 128, 64 + col_num * 128), color=255)
@@ -27,12 +30,12 @@ img_new = Image.new(mode='L', size=(
 for char in chars:
     font = ImageFont.truetype('fonts_reserve/于洪亮钢笔楷书.ttf', 32)
     draw = ImageDraw.Draw(img_new)
-    text_w, text_h = draw.textsize('raw_img', font)
+    text_w, text_h = draw.textsize('input', font)
     draw.text((64 - text_w / 2, 32 - text_h / 2),
-              'raw_img', font=font, fill=0)
-    text_w, text_h = draw.textsize('real_img', font)
+              'input', font=font, fill=0)
+    text_w, text_h = draw.textsize('ground\ntruth', font)
     draw.text(((row_num - 1) * 128 + 64 - text_w / 2, 32 - text_h / 2),
-              'real_img', font=font, fill=0)
+              'ground\ntruth', font=font, fill=0)
 
     img = Image.open(in_dir + '/' + char + 'raw_img.png')
     img_new.paste(img, (0, 64 + i * 128))
