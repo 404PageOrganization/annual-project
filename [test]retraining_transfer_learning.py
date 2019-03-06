c157af2bf91ab40d2e6892e62bee0d3234b027fb
cometeme <cometemeJackson@outlook.com>
2019-03-06 10:41:06 +0800

    update test model
    Former-commit-id: 3e0b9847f54f7d4f3a6d2234bdf40fd381a572e9

--- [test]retraining_transfer_learning.py
+++ [test]retraining_transfer_learning.py
@@ -15,7 +15,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
 # Define abspaths
-fonts_dir = 'fonts'
+fonts_dir = 'raw_fonts'
 target_img_dir = 'target_img'
 fake_img_dir = 'fake_img'
 pretraining_data_dir = 'model_data/pretraining.h5'
