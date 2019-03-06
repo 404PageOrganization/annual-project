cc91218e790ff750741d3e150697ad40203fe9ca
cometeme <cometemeJackson@outlook.com>
2019-03-06 12:12:28 +0800

    update test fonts
    Former-commit-id: 0061dd9e24ae01772ad25542362134cf6e7c3cd3

--- target_font_to_png.py
+++ target_font_to_png.py
@@ -8,7 +8,7 @@ fonts_dir = 'target_fonts'
 img_dir = 'target_img'
 
 # Read 3500 most used chinese characters
-characters = open('characters.txt', 'r',
+characters = open('test.txt', 'r',
                   encoding='utf-8').read().replace('\n', '')
 
 # Make all directories
